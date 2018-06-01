67894c9340b0b8665cd93e14110c051b25e709ab
Rafael Bermúdez <rbermudez@lidersoft.com>
2018-06-01 08:42:49 -0600

    Se agrega el App

---
+++ app/src/main/java/com/isthmus/rr/isthgreen/MainActivity.java
@@ -0,0 +1,29 @@
+package com.isthmus.rr.isthgreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView title;
+    private Button clickMeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        title = (TextView) findViewById(R.id.title);
+        title.setText("Curso de Android");
+        clickMeButton = (Button) findViewById(R.id.buttonClickMe);
+        clickMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello Toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
